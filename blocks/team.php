16d8f6212bb7dc18a797d59be280ee5039cc67f2
Magnus Westhofen <m.westhofen@12-05.de>
2022-12-06 12:05:24 +0100

    test

--- blocks/team.php
+++ blocks/team.php
@@ -8,7 +8,9 @@
                 <a href="<?php echo $member['link'];?>" class="member">
                     <figure>
                         <img class="profile" src="<?php echo $member['bild'];?>" alt="<?php echo $member['name'];?>" />
-                        <img class="add" src="<?php echo SMINURL.'/assets/img/add-orange.svg';?>" alt="<?php echo $member['name'];?>" />
+                        <svg class="add" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44">
+  <path id="icons8-add" d="M24,2A22,22,0,1,0,46,24,22,22,0,0,0,24,2Zm8.8,24.2H26.2v6.6H21.8V26.2H15.2V21.8h6.6V15.2h4.4v6.6h6.6Z" transform="translate(-2 -2)" fill="<?php echo get_field('farbe', 'option');?>"/>
+</svg>
 
                     </figure>
                     <h3><?php echo $member['name'];?></h3>
