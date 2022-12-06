ec73c3991b9cbfddf3ed4bc69d391d014ec1594e
Magnus Westhofen <m.westhofen@12-05.de>
2022-12-06 12:02:28 +0100

    test

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <?php wp_head();?>
+        <title><?php wp_title(''); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     </head>
