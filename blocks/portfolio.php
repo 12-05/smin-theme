589cf559d12b3521a3a43fd81e65f8b39eee5bf4
Magnus Westhofen <m.westhofen@12-05.de>
2023-01-09 15:14:10 +0100

    test

--- blocks/portfolio.php
+++ blocks/portfolio.php
@@ -8,11 +8,18 @@
             <h3>Unternehmensbeteiligungen</h3>
            <div class="grid">
                 <?php $unternehmen = get_field('unternehmen');
-                    if($unternehmen):foreach($unternehmen as $company):?>
-                    <a class="unternehmen" style="background-image:url(<?php echo wp_get_attachment_image_src(get_field('logo', $company->ID), 'full')[0];?>)" href="<?php echo get_permalink($company->ID);?>">
-                        <div class="inner">
-                            <?php echo get_the_title($company->ID);?>
-                        </div>
+                  
+
+                    if($unternehmen):foreach($unternehmen as $company):
+                        $thumbnailURL = wp_get_attachment_image_src(get_field('logo', $company->ID), 'medium')[0];
+                    ?>
+                    <a class="unternehmen" href="<?php echo get_permalink($company->ID);?>">
+                    <div class="front" style="background-image:url(<?php echo $thumbnailURL;?>)"></div>
+    
+                    <div class="inner">
+                            <strong><?php echo get_the_title($company->ID);?></strong>
+                            <br /><?php echo get_field('kurztext', $company->ID);?>
+                    </div>
                     </a>
                     <?php endforeach;endif;?>
             </div>
@@ -24,9 +31,11 @@
                     if($immobilien):foreach($immobilien as $immobilie):?>
                     <?php $thumbnailURL = wp_get_attachment_image_src(get_field('thumbnail', $immobilie->ID), 'medium')[0];?>
                     <?php if(!$thumbnailURL) continue;?>
-                    <a class="immobilie" style="background-image:url(<?php echo $thumbnailURL;?>)" href="<?php echo get_permalink($immobilie->ID);?>">
+                    <a class="immobilie"  href="<?php echo get_permalink($immobilie->ID);?>">
+                    <div class="front" style="background-image:url(<?php echo $thumbnailURL;?>)"> </div>
                     <div class="inner">
-                            <?php echo get_the_title($immobilie->ID);?>
+                    <strong><?php echo get_the_title($immobilie->ID);?></strong>
+                            <br /><?php echo get_field('kurztext', $immobilie->ID);?>
                         </div>
                     </a>
                     <?php endforeach;endif;?>
