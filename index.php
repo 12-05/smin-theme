5600385a6c6e74fd431c256b3a652778582ba9fb
Liu Yukun <liuyukun@MacBook-Pro-von-Liu.local>
2023-09-08 16:41:57 +0200

    pagnation

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <h1>News</h1>
         <?php  get_template_part('loop');?>
     </div>
-    <div class="pagination">
+    <div class="pagination" style='text-align:center;margin-bottom:32px;margin-top:-96px;'>
         <?php echo paginate_links(); ?>
     </div>
     <?php get_footer();
