0cb35b1f3c433d57c4c723192b92330e09bea46b
Magnus Westhofen <m.westhofen@12-05.de>
2023-10-04 10:11:06 +0200

    added wrapper

--- blocks/team.php
+++ blocks/team.php
@@ -39,7 +39,7 @@
             <? endforeach;?>
 
         </div>
-        <div style="margin-top:2rem">Und viele mehr ...</div>
+        <div class="wrapped" style="margin-top:2rem">Und viele mehr ...</div>
 
 <?php endif;?>
     </div>
